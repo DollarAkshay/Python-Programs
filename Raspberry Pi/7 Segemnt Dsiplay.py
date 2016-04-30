1dc4eb683ba6154c48f8b18477eba621139ebfb4
DollarAkshay <akshay95aradhya@gmail.com>
2016-04-30 12:47:29 +0000

    Adding a video link for 7 Segment Display :cyclone:

--- Raspberry Pi/7 Segemnt Dsiplay.py
+++ Raspberry Pi/7 Segemnt Dsiplay.py
@@ -3,7 +3,7 @@ import time
 
 segment = [33, 31, 38, 36, 32, 35, 37, 40]
 
-
+# Link to Video : https://www.youtube.com/watch?v=l2irGcM5Wf4
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(segment, GPIO.OUT, initial = GPIO.LOW)
@@ -13,7 +13,7 @@ try:
         for pin in segment:
             GPIO.output(pin, 1)
             time.sleep(0.25)
-        time.sleep(5)
+        time.sleep(1)
         for pin in segment:
             GPIO.output(pin, 0)
             time.sleep(0.25)
