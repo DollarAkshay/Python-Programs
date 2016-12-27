0716008d2bd0269c9b94b21639c9975f2694c953
dollarakshay <akshay95aradhya@gmail.com>
2016-12-27 16:36:43 +0530

    Adding Mountain Car (Fail) :car:

--- Artificial Intelligence/OpenAI_MountainCar_v0.py
+++ Artificial Intelligence/OpenAI_MountainCar_v0.py
@@ -1,318 +1,112 @@
-import time, math, random, bisect, copy
+import time, random, math
 import gym
 import numpy as np
+import tensorflow as tf
 
 
-class NeuralNet : 
-    def __init__(self, nodeCount, loadFile):     
-        self.fitness = 0.0
-        self.nodeCount = nodeCount
-        self.weights = []
-        self.biases = []
-
-        if loadFile:
-            self.nodeCount, self.weights, self.biases = loadWeights()
-        else:
-            for i in range(len(nodeCount) - 1):
-                self.weights.append( np.random.uniform(low=-1, high=1, size=(nodeCount[i], nodeCount[i+1])).tolist() )
-                self.biases.append( np.random.uniform(low=-1, high=1, size=(nodeCount[i+1])).tolist())
-
-
-    def printWeightsandBiases(self):
-        
-        print("--------------------------------")
-        print("Weights :\n[", end="")
-        for i in range(len(self.weights)):
-            print("\n [ ", end="")
-            for j in range(len(self.weights[i])):
-                if j!=0:
-                    print("\n   ", end="")
-                print("[", end="")
-                for k in range(len(self.weights[i][j])):
-                    print(" %5.2f," % (self.weights[i][j][k]), end="")
-                print("\b],", end="")
-            print("\b ],")
-        print("\n]")
-
-        print("\nBiases :\n[", end="")
-        for i in range(len(self.biases)):
-            print("\n [ ", end="")
-            for j in range(len(self.biases[i])):
-                    print(" %5.2f," % (self.biases[i][j]), end="")
-            print("\b],", end="")
-        print("\b \n]\n--------------------------------\n")
-  
-    def getOutput(self, input):
-        output = input
-        for i in range(len(self.nodeCount)-1):
-            output = np.reshape( np.matmul(output, self.weights[i]) + self.biases[i], (self.nodeCount[i+1]))
-            output = np.maximum(output, 0)
-        return np.argmax(output)
-
-
-class Population :
-    def __init__(self, populationCount, mutationRate, nodeCount, loadFile=False):
-        self.nodeCount = nodeCount
-        self.popCount = populationCount
-        self.m_rate = mutationRate
-        self.population = [ NeuralNet(nodeCount, loadFile) for i in range(populationCount)]
-
-
-    def createChild(self, nn1, nn2):
-        
-        child = NeuralNet(self.nodeCount, False)
-        for i in range(len(child.weights)):
-            for j in range(len(child.weights[i])):
-                for k in range(len(child.weights[i][j])):
-                    if random.random() > self.m_rate:
-                        if random.random() < nn1.fitness / (nn1.fitness+nn2.fitness):
-                            child.weights[i][j][k] = nn1.weights[i][j][k]
-                        else :
-                            child.weights[i][j][k] = nn2.weights[i][j][k]
-                        
-
-        for i in range(len(child.biases)):
-            for j in range(len(child.biases[i])):
-                if random.random() > self.m_rate:
-                    if random.random() < nn1.fitness / (nn1.fitness+nn2.fitness):
-                        child.biases[i][j] = nn1.biases[i][j]
-                    else:
-                        child.biases[i][j] = nn2.biases[i][j]
-
-        return child
-
-
-    def createNewGeneration(self):    
-        nextGen = []
-        self.population.sort(key=lambda x: x.fitness, reverse=True)
-        for i in range(self.popCount):
-            if random.random() < float(self.popCount-i)/self.popCount:
-                nextGen.append(copy.deepcopy(self.population[i]));
-
-        fitnessSum = [0]
-        minFit = min([i.fitness for i in nextGen])
-        for i in range(len(nextGen)):
-            fitnessSum.append(fitnessSum[i]+(nextGen[i].fitness-minFit)**4)
-        
-
-        while(len(nextGen) < self.popCount):
-            r1 = random.uniform(0, fitnessSum[len(fitnessSum)-1] )
-            r2 = random.uniform(0, fitnessSum[len(fitnessSum)-1] )
-            i1 = bisect.bisect_left(fitnessSum, r1)
-            i2 = bisect.bisect_left(fitnessSum, r2)
-            if 0 <= i1 < len(nextGen) and 0 <= i2 < len(nextGen) :
-                nextGen.append( self.createChild(nextGen[i1], nextGen[i2]) )
-            else :
-                print("Index Error ");
-                print("Sum Array =",fitnessSum)
-                print("Randoms = ", r1, r2)
-                print("Indices = ", i1, i2)
-        self.population.clear()
-        self.population = nextGen
-
-
-def sigmoid(x):
-    return 1.0/(1.0 + np.exp(-x))
-
-def mapRange(value, leftMin, leftMax, rightMin, rightMax):
-    # Figure out how 'wide' each range is
-    leftSpan = leftMax - leftMin
-    rightSpan = rightMax - rightMin
-
-    # Convert the left range into a 0-1 range (float)
-    valueScaled = float(value - leftMin) / float(leftSpan)
-
-    # Convert the 0-1 range into a value in the right range.
-
-    return rightMin + (valueScaled * rightSpan)
-
-def normalizeArray(aVal, aMin, aMax): 
-    res = []
-    for i in range(len(aVal)):
-        res.append( mapRange(aVal[i], aMin[i], aMax[i], -1, 1) )
-    return res
-
-def scaleArray(aVal, aMin, aMax):   
-    res = []
-    for i in range(len(aVal)):
-        res.append( mapRange(aVal[i], -1, 1, aMin[i], aMax[i]) )
-    return res    
-
-def replayBestBots(bestNeuralNets, steps, sleep):  
-    choice = input("Do you want to watch the replay ?[Y/N] : ")
-    if choice=='Y' or choice=='y':
-        for i in range(len(bestNeuralNets)):
-            if (i+1)%steps == 0 :
-                observation = env.reset()
-                totalReward = 0
-                for step in range(MAX_STEPS):
-                    env.render()
-                    time.sleep(sleep)
-                    action = bestNeuralNets[i].getOutput(observation)
-                    observation, reward, done, info = env.step(action)
-                    totalReward += reward
-                    if done:
-                        break
-                print("Generation %3d | Expected Fitness of %4d | Actual Fitness = %4d" % (i+1, bestNeuralNets[i].fitness, totalReward))
-
-
-def recordBestBots(bestNeuralNets):  
-    print("\n Recording Best Bots ")
-    print("---------------------")
-    env.monitor.start('Artificial Intelligence/'+GAME, force=True)
-    observation = env.reset()
-    for i in range(len(bestNeuralNets)):
-        totalReward = 0
-        for step in range(MAX_STEPS):
-            env.render()
-            action = bestNeuralNets[i].getOutput(observation)
-            observation, reward, done, info = env.step(action)
-            totalReward += reward
-            if done:
-                observation = env.reset()
-                break
-        print("Generation %3d | Expected Fitness of %4d | Actual Fitness = %4d" % (i+1, bestNeuralNets[i].fitness, totalReward))
-    env.monitor.close()
-
-
-
-
-def loadWeights():
-    nodeCount = []
-    weights = []
-    biases = []
-    try :
-        f = open('Artificial Intelligence/'+GAME+"/"+GAME+".txt", 'r')
-    except FileNotFoundError:
-        print("File Not Found. Initializing random values")
-        nodeCount = node_per_layer
-        for i in range(len(nodeCount) - 1):
-            weights.append( np.random.uniform(low=-1, high=1, size=(nodeCount[i], nodeCount[i+1])).tolist() )
-            biases.append( np.random.uniform(low=-1, high=1, size=(nodeCount[i+1])).tolist())
-        return nodeCount, weights, biases
-
-
-    print("Type : ", type(f))
+def neural_network():
     
-    f.readline()
-    nodeCount = [int(i) for i in f.readline().split()]
+    weights = {
+        'hidden1': tf.Variable( tf.random_normal( [ip_features, nodeCount[0]] )) ,
+        'hidden2': tf.Variable( tf.random_normal( [nodeCount[0], nodeCount[1]] )),
+        'out'    : tf.Variable( tf.random_normal( [nodeCount[1], op_features] ))
+    }
 
-    f.readline()
-    for i in range(len(nodeCount) - 1):
-        weights.append([])
-        for j in range(nodeCount[i]):
-            weights[i].append([float(i) for i in f.readline().split()])
+    biases = {
+        'hidden1': tf.Variable( tf.random_normal( [nodeCount[0]] )) ,
+        'hidden2': tf.Variable( tf.random_normal( [nodeCount[1]] )),
+        'out'    : tf.Variable( tf.random_normal( [op_features] ))
+    }
 
-    f.readline()
-    for i in range(len(nodeCount) - 1):
-        biases.append([float(i) for i in f.readline().split()])
-    f.close()
+    hidden1 = tf.add( tf.matmul(ip_placeholder, weights['hidden1']), biases['hidden1'])
+    hidden1 = tf.nn.relu(hidden1)
 
-    return nodeCount, weights, biases
+    hidden2 = tf.add( tf.matmul(hidden1, weights['hidden2']), biases['hidden2'])
+    hidden2 = tf.nn.relu(hidden2)
 
+    output = tf.add( tf.matmul(hidden2, weights['out']), biases['out'])
 
-
-def saveWeights(best):
-    
-    f = open('Artificial Intelligence/'+GAME+"/"+GAME+".txt", 'w')
-    print("Node Count : ", file=f)
-    for i in range(len(best.nodeCount)):
-        print("%d " % best.nodeCount[i], file=f, end="");
-    print("", file=f)
-
-    print("Weights : ", file=f)
-    for i in range(len(best.weights)):
-            for j in range(len(best.weights[i])):
-                for k in range(len(best.weights[i][j])):
-                    print("%+.2f " % best.weights[i][j][k], file=f, end="")
-                print("", file=f)
-
-    print("Biases : ", file=f)
-    for i in range(len(best.biases)):
-            for j in range(len(best.biases[i])):
-                print("%+.2f " % best.biases[i][j], file=f, end="")
-            print("", file=f)
-
-    f.close()
-
+    return output
 
 def uploadSimulation():
     API_KEY = open('/home/dollarakshay/Documents/API Keys/Open AI Key.txt', 'r').read().rstrip()
-    gym.upload('Artificial Intelligence/'+GAME, api_key=API_KEY)    
+    gym.upload('Artificial Intelligence/'+GAME, api_key=API_KEY)
 
+    
 
 
 GAME = 'MountainCar-v0'
 env = gym.make(GAME)
 
-MAX_STEPS = env.spec.timestep_limit
-MAX_GENERATIONS = 1000
-POPULATION_COUNT = 50
-MUTATION_RATE = 0.1
+RECORD = None
+MAX_EPISODES = 10001
+MAX_STEPS = env.spec.timestep_limit     # 100 for FrozenLake v0
+EPSILON = 0
+DISCOUNT = 0.99
+LEARNING_RATE = 0.1
 
-in_dimen = env.observation_space.shape[0]
-out_dimen = env.action_space.n
+
+ip_features = env.observation_space.shape[0]
+op_features = env.action_space.n
 obsMin = env.observation_space.low
 obsMax = env.observation_space.high
 actionMin = 0
 actionMax = env.action_space.n - 1 
-node_per_layer = [in_dimen, 5 , out_dimen]
 
-pop = Population(POPULATION_COUNT, MUTATION_RATE, node_per_layer, False)
-bestNeuralNets = []
+nodeCount = [5, 5]
+ip_placeholder = tf.placeholder('float', [1, ip_features])
+op_placeholder = tf.placeholder('float', [1, op_features])
 
 print("\nGENERAL\n--------------------------------")
 print("Max Steps :", MAX_STEPS)
 print("\nOBSERVATION\n--------------------------------")
-print("Shape :", in_dimen, " High :", obsMax, " Low :", obsMin)
+print("Shape :", ip_features, " High :", obsMax, " Low :", obsMin)
 print("\nACTION\n--------------------------------")
-print("Shape :", out_dimen, " | High :", actionMax, " | Low :", actionMin,"\n")
+print("Shape :", op_features, " | High :", actionMax, " | Low :", actionMin,"\n")
 
-try :
-    for gen in range(MAX_GENERATIONS):
-        genAvgFit = 0.0
-        minFit =  1000000
-        maxFit = -1000000
-        maxNeuralNet = None
-        for cr, nn in enumerate(pop.population):
-            observation = env.reset()
-            totalReward = 0
-            for step in range(MAX_STEPS):
-                if cr==0:
-                    env.render()
-                action = nn.getOutput(observation)
-                observation, reward, done, info = env.step(action)
-                totalReward += reward
-                if done:
-                    break
 
-            nn.fitness = totalReward
-            minFit = min(minFit, nn.fitness)
-            genAvgFit += nn.fitness
-            if nn.fitness > maxFit :
-                maxFit = nn.fitness
-                maxNeuralNet = copy.deepcopy(nn);
+#env.monitor.start('Artificial Intelligence/'+GAME, force=True, video_callable=RECORD)
 
-        bestNeuralNets.append(maxNeuralNet)
-        genAvgFit/=pop.popCount
-        print("Generation : %3d  |  Min : %5.0f  |  Avg : %5.0f  |  Max : %5.0f  " % (gen+1, minFit, genAvgFit, maxFit) )
-        pop.createNewGeneration()
 
-    #recordBestBots(bestNeuralNets)
-    #uploadSimulation()
-    #replayBestBots(bestNeuralNets, max(1, int(math.ceil(MAX_GENERATIONS/10.0))), 0)
+output_prediction = neural_network()
+output_action = tf.argmax(output_prediction, 1)[0]
+output_target = tf.placeholder('float', [1, op_features])
+loss = tf.reduce_sum(tf.square(output_target - output_prediction))
+trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+optimizer = trainer.minimize(loss)
 
-except KeyboardInterrupt:
-    print("\nKeyboard Interrupt")
-except Exception as e: 
-    print (str(e))
-finally :
-    if len(bestNeuralNets) > 1:
-        print("\nSaving Weights to file")
-        saveWeights(bestNeuralNets[-1])
-    else:
-        print("No weights to save")
-    print("\nQuitting")
+with tf.Session() as sess :
+    sess.run(tf.global_variables_initializer())
+    for episode in range(MAX_EPISODES):
+        curState = env.reset()
+        totalReward = 0
+        for step in range(MAX_STEPS):
+            if episode%100 == 0:
+                env.render()
+            action, actualQ = sess.run([output_action, output_prediction], feed_dict={ip_placeholder: np.reshape(curState, [1, 2]) })
+            
+            curState, reward, done, info = env.step(action)
+            totalReward += reward
+            maxQCurState = np.max(sess.run(output_prediction, feed_dict={ip_placeholder: np.reshape(curState, [1, 2]) }))
+            targetQ = actualQ
+            targetQ[0, action] = reward + DISCOUNT*maxQCurState
+
+            _, l = sess.run([optimizer, loss],feed_dict={ip_placeholder: np.reshape(curState, [1, 2]) , output_target: targetQ})
+            if done :
+                break
+        print("Episode %d wit a reward of %d" % (episode, totalReward))
+
+
+#env.monitor.close()
+
+uploadSimulation()
+
+
+
+
+
+
+
 
 
 
