925d4ef43ecac306d00e6296bc57dbc2c45062b2
dollarakshay <akshay95aradhya@gmail.com>
2016-12-31 01:42:58 +0530

    Commit before sleeping

--- Artificial Intelligence/Kaggle_Dogs_vs_Cats.py
+++ Artificial Intelligence/Kaggle_Dogs_vs_Cats.py
@@ -93,11 +93,12 @@ def train_neural_network():
 
     with tf.Session() as sess :
         sess.run(tf.global_variables_initializer())
+        
 
         # Train Neural Net
         print("\nTraining Neural Net ... ")
         try :
-            ephocs = 20
+            ephocs = 200
             for epoch in range(ephocs):
                 epoch_loss = 0
                 i = 0
