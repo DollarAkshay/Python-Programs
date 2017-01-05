a2dd83f7eee813ffb84731d657c3cc90e81c3996
dollarakshay <akshay95aradhya@gmail.com>
2017-01-05 21:35:21 +0530

    God only knows whats wrong with this :disappointed:

--- Competitive Coding/HackerRank_The_Bidding_Game.py
+++ Competitive Coding/HackerRank_The_Bidding_Game.py
@@ -27,13 +27,20 @@ class GameState():
         board[self.pos] = 1
         state = np.append(state, board)
         if self.drawFavor == 1:
-            state = np.append(state, -1)
+            state = np.append(state, 0)
         else:
             state = np.append(state, 1)
 
         return np.reshape(state, (1, ip_features))
 
-    def step(self, bet1, bet2):     
+    def step(self, bet1, bet2):
+
+        if (bet1 > self.player1Cash) or (bet1==0 and self.player1Cash>0):
+            return (True, [-REWARD, REWARD])   
+
+        if (bet2 > self.player2Cash) or (bet2==0 and self.player2Cash>0):
+            return (True, [REWARD, -REWARD])
+
         if bet1 > bet2 :
             self.pos-=1
             self.player1Cash -= bet1
@@ -50,47 +57,19 @@ class GameState():
             self.drawFavor = 1
 
         if self.pos==0:
-            return (True, [100, -100])
+            return (True, [REWARD, -REWARD])
         elif self.pos==10:
-            return (True, [-100, 100])
+            return (True, [-REWARD, REWARD])
         elif self.player1Cash==0 and self.player2Cash==0:
-            return (True, [5-self.pos, self.pos-5])
+            return (True, [0, 0])
         else:
             return (False, [0, 0])
 #
-# Player 1 Neural Network
-def neural_network1():
-    
-    nodeCount = [10, 10]
-
-    weights = {
-        'hidden1': tf.Variable( tf.random_normal( [ip_features, nodeCount[0]] )) ,
-        'hidden2': tf.Variable( tf.random_normal( [nodeCount[0], nodeCount[1]] )),
-        'out'    : tf.Variable( tf.random_normal( [nodeCount[1], op_features] ))
-    }
-
-    biases = {
-        'hidden1': tf.Variable( tf.random_normal( [nodeCount[0]] )) ,
-        'hidden2': tf.Variable( tf.random_normal( [nodeCount[1]] )),
-        'out'    : tf.Variable( tf.random_normal( [op_features] ))
-    }
-
-    hidden1 = tf.add( tf.matmul(ip_placeholder, weights['hidden1']), biases['hidden1'])
-    hidden1 = tf.nn.sigmoid(hidden1)
-
-    hidden2 = tf.add( tf.matmul(hidden1, weights['hidden2']), biases['hidden2'])
-    hidden2 = tf.nn.sigmoid(hidden2)
-
-    output = tf.add( tf.matmul(hidden2, weights['out']), biases['out'])
-    output = tf.nn.sigmoid(output)
-    output = tf.matmul(output, limit_placeholder)
-    
-    return output
 #
 # Player 2 Neural Network
-def neural_network2():
+def neural_network():
     
-    nodeCount = [180, 120]
+    nodeCount = [150, 120]
 
     weights = {
         'hidden1': tf.Variable( tf.random_normal( [ip_features, nodeCount[0]] )) ,
@@ -105,136 +84,85 @@ def neural_network2():
     }
 
     hidden1 = tf.add( tf.matmul(ip_placeholder, weights['hidden1']), biases['hidden1'])
-    hidden1 = tf.nn.sigmoid(hidden1)
+    hidden1 = tf.nn.relu(hidden1)
 
     hidden2 = tf.add( tf.matmul(hidden1, weights['hidden2']), biases['hidden2'])
     hidden2 = tf.nn.sigmoid(hidden2)
 
     output = tf.add( tf.matmul(hidden2, weights['out']), biases['out'])
     output = tf.nn.sigmoid(output)
-    output = tf.matmul(output, limit_placeholder)
     
     return output
-#
-# Calc Bet Limit
-def getBetLimit(state, player):
-    playerCash = 0
-    for i in range(op_features):
-        if state[0][(player-1)*op_features +i] > 0:
-            playerCash = i
-    
-    res = np.eye(op_features)
-    for i in range(op_features):
-        if i > playerCash:
-            res[i][i] = 0
-
-    if playerCash > 0:
-        res[0][0] = 0
-    return res
 
 #
 # Main
-EPISODES = 10000
+EPISODES = 100000
 EPSILON = 0.1
-DISCOUNT = 0.99
+DISCOUNT = 0.9
+LEARNING_RATE = 1
+REWARD = 1
 
-
-op_features = 101
+op_features = 20
 ip_features = op_features*2+11+1
 ip_placeholder = tf.placeholder('float', [1, ip_features])
 op_placeholder = tf.placeholder('float', [1, op_features])
-limit_placeholder = tf.placeholder('float', [op_features, op_features])
 
 game = GameState()
 
-output_prediction1 = neural_network1()
-output_action1 = tf.argmax(output_prediction1, 1)[0]
-output_target1 = tf.placeholder('float', [1, op_features])
-loss1 = tf.reduce_sum(tf.square(output_target1 - output_prediction1))
-trainer1 = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-optimizer1 = trainer1.minimize(loss1)
-
-output_prediction2 = neural_network2()
-output_action2 = tf.argmax(output_prediction2, 1)[0]
-output_target2 = tf.placeholder('float', [1, op_features])
-loss2 = tf.reduce_sum(tf.square(output_target2 - output_prediction2))
-trainer2 = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-optimizer2 = trainer2.minimize(loss2)
+output_prediction = neural_network()
+output_action = tf.argmax(output_prediction, 1)[0]
+output_target = tf.placeholder('float', [1, op_features])
+loss = tf.reduce_sum(tf.square(output_target - output_prediction))
+trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+optimizer = trainer.minimize(loss)
 
 np.set_printoptions(precision=3, suppress=True, linewidth=120)
 
 with tf.Session() as sess :
     sess.run(tf.global_variables_initializer())
+    winarray = []
+
     for episode in range(EPISODES):
         print("\n\nGame ", episode)
-        game.reset()
         
-        totalLoss1 = 0
-        totalLoss2 = 0
+        game.reset()
+        totalLoss = 0
         for turn in range(1000):
             #print("Turn :", turn)
             curState = game.getState()
             
             
-            betlimit1 = getBetLimit(curState, 1)
-            action1, actualQ1 = sess.run([output_action1, output_prediction1], feed_dict={ip_placeholder: curState, limit_placeholder: betlimit1 })
-            if np.random.rand(1) < 1 and game.player1Cash > 0:
-                #print("Player 1 took random action")
+            action1 = int(game.player1Cash/game.pos)
+            if np.random.rand(1) < 0.2  and game.player1Cash > 0: 
                 action1 = random.randint(1, game.player1Cash)
-            
 
-            betlimit2 = getBetLimit(curState, 2)
-            action2, actualQ2 = sess.run([output_action2, output_prediction2], feed_dict={ip_placeholder: curState, limit_placeholder: betlimit2 })
+            action2, actualQ = sess.run([output_action, output_prediction], feed_dict={ip_placeholder: curState })
             if np.random.rand(1) < EPSILON  and game.player2Cash > 0: 
-                #print("Player 2 took random action")
                 action2 = random.randint(1, game.player2Cash)
-            
-
-            if episode%1000==0:
-                for i in range(11):
-                    print("#", end=" ") if i == game.pos else print("_", end=" ")    
-                print("")
-                print("Player 1 bet "+str(action1)+"/"+str(game.player1Cash))
-                print("Player 2 bet "+str(action2)+"/"+str(game.player2Cash))
-
 
             prevState = curState
             done, rewards = game.step(action1, action2)
             curState = game.getState()
 
 
-            betlimit1 = getBetLimit(curState, 1)
-            betlimit2 = getBetLimit(curState, 2)
+            maxQCurState = np.max(sess.run(output_prediction, feed_dict={ip_placeholder: curState}))
 
-            maxQ1CurState = np.max(sess.run(output_prediction1, feed_dict={ip_placeholder: curState, limit_placeholder: betlimit1 }))
-            maxQ2CurState = np.max(sess.run(output_prediction2, feed_dict={ip_placeholder: curState, limit_placeholder: betlimit2 }))
 
-            betlimit1 = getBetLimit(prevState, 1)
-            betlimit2 = getBetLimit(prevState, 2)
 
-            targetQ1 = actualQ1
-            targetQ1[0, action1] = rewards[0] + DISCOUNT*maxQ1CurState
+            targetQ = actualQ
+            targetQ[0, action2] = LEARNING_RATE*(rewards[1] + DISCOUNT*maxQCurState - targetQ[0, action2])
 
-            targetQ2 = actualQ2
-            targetQ2[0, action2] = rewards[1] + DISCOUNT*maxQ2CurState
-
-            _, l1 = sess.run([optimizer1, loss1],feed_dict={ip_placeholder: prevState , output_target1: targetQ1, limit_placeholder: betlimit1})
-            totalLoss1+=l1
-            _, l2 = sess.run([optimizer2, loss2],feed_dict={ip_placeholder: prevState , output_target2: targetQ2, limit_placeholder: betlimit2})
-            totalLoss2+=l2
-
-            if episode%1000==0:
-                input()
+            _, l = sess.run([optimizer, loss],feed_dict={ip_placeholder: prevState , output_target: targetQ})
+            totalLoss+=l
 
             if done:
-                if max(rewards)==100 and episode%1000==0:
-                    print(" ~~~ PLAYER", np.argmax(rewards)+1,"WINS ~~~")
-                    
+                if max(rewards)==REWARD :
+                    winarray.append( 1 if rewards[1]==REWARD else 0 )
+                    if len(winarray)>100:
+                        winarray.pop(0)
                 break
         
-        print("Total Loss for Player 1 :", totalLoss1)
-        print("Total Loss for Player 2 :", totalLoss2)
-        if episode%1000==0:
-            input()
-        EPSILON-=0.1/100
+        print("Total Loss for Player 2 :", totalLoss)
+        print(" Player 2 wins: "+str(sum(winarray))+" / 100")
+        EPSILON-=0.1/1000
 
