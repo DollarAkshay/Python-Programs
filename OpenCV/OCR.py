593243ae59641bcf37fc1ca90e034c639fa1c534
DollarAkshay <akshay95aradhya@gmail.com>
2017-04-08 21:04:53 +0530

    Adding OpenCV Programs

---
+++ OpenCV/OCR.py
@@ -0,0 +1,7 @@
+import pytesseract 
+from PIL import Image
+import cv2
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
+
+print(pytesseract.image_to_string(Image.open('OpenCV/Images/card.png')))
