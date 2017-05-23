0e79907773d1d9fa5453309c7a6f1c5d06560275
DollarAkshay <akshay95aradhya@gmail.com>
2017-05-23 11:40:52 +0530

    Addign a silly Graduation List Sorting Program

---
+++ Miscellaneous/Sort_File_By_Line.py
@@ -0,0 +1,23 @@
+
+
+
+
+f = open('Miscellaneous/Data/Graduation_Unordered.txt')
+nameList = []
+for line in f:
+    line = line[3:-1]
+    nameList.append(line)
+
+nameList.sort()
+f.close()
+f = open('Miscellaneous/Data/Gradution_Ordered.txt', 'w')
+
+for i, name in enumerate(nameList):
+    number = str(i+1)
+    if i<9:
+        number = "0"+number
+    if name=="Akshay L Aradhya":
+        name = "*"+name+"*"
+    f.write(number+" "+name+"\n")
+
+f.close()
