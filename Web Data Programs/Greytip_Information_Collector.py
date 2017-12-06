72d06dab1230cbfc7a5eeb307f315ff150fca9a9
DollarAkshay <akshay95aradhya@gmail.com>
2017-12-07 02:58:09 +0530

    Mongo DB Implementation

--- Web Data Programs/Greytip_Information_Collector.py
+++ Web Data Programs/Greytip_Information_Collector.py
@@ -1,4 +1,4 @@
-import MySQLdb as db
+
 import os
 import sys
 import json
@@ -7,36 +7,64 @@ import urllib.request
 import urllib.parse
 import time
 
-file = open("C:\\Users\\akshay.aradhya\\Documents\\API Keys\\Server_Credentials.txt", "r")
-HOST = file.readline().rstrip('\n')
-PORT = 3306
-USER = file.readline().rstrip('\n')
-PASS = file.readline().rstrip('\n')
-DB = "betsol"
+from pymongo import MongoClient
 
-connection = db.Connection(host=HOST, port=PORT, user=USER, passwd=PASS, db=DB)
-cursor = connection.cursor()
 
-file = open("C:\\Users\\akshay.aradhya\\Documents\\API Keys\\Betsol_Greytip_Key.txt", "r")
-session_key = file.read().rstrip('\n')
-file.close()
-print(session_key)
+def getMongoDBConnection():
+    try:
+        print("Connecting to MongoDB server...", end="")
+        client = MongoClient()
+        file = open(
+            "C:\\Users\\akshay.aradhya\\Documents\\API Keys and Credentials\\mongo_db.txt", "r")
+        HOST = file.readline().rstrip('\n')
+        PORT = 27017
+        USER = file.readline().rstrip('\n')
+        PASS = file.readline().rstrip('\n')
+        DB = "betsol"
 
+        client = MongoClient(HOST, PORT, username=USER, password=PASS)
+        db = client[DB]
+        print("Success")
+        return db
+    except Exception as e:
+        print("Failed")
+        print(str(e))
+
+
+def getSessionKey():
+    print("Fetching Session key...", end="")
+    file = open(
+        "C:\\Users\\akshay.aradhya\\Documents\\API Keys and Credentials\\Betsol_Greytip_Key.txt", "r")
+    session_key = file.read().rstrip('\n')
+    print(session_key)
+    return session_key
+
+
+def fetchEmployeeList():
+    print("Getting Employee List...", end="")
+    req = urllib.request.Request(
+        "https://betsol.greytip.in/v2/employee/directory?page=0&pageLimit=2000&_=" + timestamp)
+    req.add_header('Cookie', session_key)
+    response = urllib.request.urlopen(req)
+    data = response.read().decode("utf-8")
+    emp_list = json.loads(str(data))
+    print("Success")
+    return emp_list
+
+
+start_time = time.time()
+db = getMongoDBConnection()
+session_key = getSessionKey()
 timestamp = str(int(time.time()))
 
-req = urllib.request.Request("https://betsol.greytip.in/v2/employee/directory?page=0&pageLimit=2000&_=" + timestamp)
-req.add_header('Cookie', session_key)
-response = urllib.request.urlopen(req)
-data = response.read().decode("utf-8")
-emp_list = json.loads(str(data))
-
-print("Loaded Employee List")
+emp_list = fetchEmployeeList()
 
 
 for emp in emp_list['employees']:
     try:
-        print("Updating info for " + str(emp['cid']), end="...")
-        req = urllib.request.Request("https://betsol.greytip.in/v2/employee/directory/details/" + str(emp['cid']) + "?_=" + timestamp)
+        print("Updating info for " + str(emp['name']), end="...")
+        req = urllib.request.Request(
+            "https://betsol.greytip.in/v2/employee/directory/details/" + str(emp['cid']) + "?_=" + timestamp)
         req.add_header('Cookie', session_key)
         response = urllib.request.urlopen(req)
         data = response.read().decode("utf-8")
@@ -45,25 +73,49 @@ for emp in emp_list['employees']:
         if emp['email'] is None:
             emp['email'] = "None"
 
-        if emp_info['records'][1]['value'] is None:
-            emp_dob = "None"
-        else:
-            emp_dob = datetime.datetime.strptime(emp_info['records'][1]['value'] + ' 2000', '%d %b %Y').strftime('%Y-%m-%d')
+        for record in emp_info['records']:
+            if record['title'] == 'Joining Date':
+                if record['value'] is None:
+                    emp['doj'] = "None"
+                else:
+                    emp['doj'] = datetime.datetime.strptime(
+                        record['value'], '%d %b %Y').strftime('%Y-%m-%d')
+            elif record['title'] == 'Date Of Birth':
+                if record['value'] is None:
+                    emp['dob'] = "None"
+                else:
+                    emp['dob'] = datetime.datetime.strptime(
+                        record['value'] + ' 2000', '%d %b %Y').strftime('%Y-%m-%d')
+            elif record['title'] == 'Reporting To':
+                if record['value'] is None:
+                    emp['manager_id'] = None
+                else:
+                    manager = record['value']
+                    emp['manager_id'] = manager[manager.find(
+                        "[") + 1: manager.find("]")]
+            elif record['title'] == 'Location':
+                if record['value'] is None:
+                    emp['location'] = None
+                else:
+                    emp['location'] = record['value']
+            elif record['title'] == 'Designation':
+                if record['value'] is None:
+                    emp['designation'] = None
+                else:
+                    emp['designation'] = record['value']
+            elif record['title'] == 'Department':
+                if record['value'] is None:
+                    emp['department'] = "None"
+                else:
+                    emp['department'] = record['value']
 
-        emp_jod = datetime.datetime.strptime(emp_info['records'][0]['value'], '%d %b %Y').strftime('%Y-%m-%d')
-        emp_dept = emp_info['records'][4]['value']
-        if emp_dept is None:
-            emp_dept = "None"
-        emp_loc = emp_info['records'][6]['value']
-
-        sql = "INSERT INTO `Employees` VALUES ( " + str(emp['cid']) + ", '" + emp['name'] + "', NULL, '" + emp['email'] + "', '" + emp['employeeno'] + \
-            "', '" + emp_dob + "', '" + emp_jod + "', '" + emp_dept + "', '" + emp['c_designation'] + "', '" + emp_loc + "') ON DUPLICATE KEY UPDATE name = name"
-        cursor.execute(sql)
-        print(" Done")
+        emp.pop('guid', None)
+        db.employee.insert_one(emp)
+        print("Done")
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         print(str(e), exc_tb.tb_lineno)
-    finally:
-        connection.commit()
 
-connection.close()
+end_time = time.time()
+print("Employee Details updated")
+print("Time Taken :", end_time - start_time, "sec")
