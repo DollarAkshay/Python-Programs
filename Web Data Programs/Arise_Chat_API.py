fc86f89c517fc8decb32aab8e299ae2b1e511e99
DollarAkshay <akshay95aradhya@gmail.com>
2017-12-11 23:47:53 +0530

    Adding Santa tracker :santa:

--- Web Data Programs/Arise_Chat_API.py
+++ Web Data Programs/Arise_Chat_API.py
@@ -5,6 +5,19 @@ import datetime
 
 
 def addUser(data):
+    '''
+    data example
+    -------------
+    {
+        'Username': 'Example',
+        'EffectiveStartDate': '2000-01-01T00:00:00.000',
+        'EffectiveEndDate': '2100-01-01T00:00:00.000',
+        'DisplayName': 'Example',
+        'UserRoleId': '3'
+        'ActiveFlag': 'Active'
+    }
+    '''
+
     url = baseURL + 'user/post'
     req = urllib.request.Request(url, json.dumps(data).encode('utf8'))
     req.add_header('Content-Type', 'application/json')
@@ -22,11 +35,23 @@ def addChannelUser(data):
     print(data)
 
 
-baseURL = 'http://192.168.10.66:85/api/'
+baseURL = 'http://192.168.20.65:85/api/'
 
-for i in range(1, 201):
-    channelUserData = {
-        'ChannelId': 2,
-        'UserId': 300 + i,
+# for i in range(101, 201):
+#     data = {
+#         'Username': 'BOT_PF_' + str(i),
+#         'EffectiveStartDate': '2000-01-01T01:01:01.000Z',
+#         'EffectiveEndDate': '2100-01-01T01:01:01.000Z',
+#         'DisplayName': 'BOT_PF_' + str(i),
+#         'UserRoleId': '2',
+#         'ActiveFlag': 'Active'
+#     }
+#     addUser(data)
+
+
+for i in range(101, 301):
+    data = {
+        'ChannelId': 1,
+        'UserId': i,
     }
-    addChannelUser(channelUserData)
+    addChannelUser(data)
---
+++ Web Data Programs/SecretSantaTracker.py
@@ -0,0 +1,104 @@
+from pymongo import MongoClient
+
+
+def getMongoDBConnection():
+    try:
+        print("Connecting to MongoDB server...", end="")
+        client = MongoClient()
+        file = open(
+            "C:\\Users\\akshay.aradhya\\Documents\\API Keys and Credentials\\mongo_db.txt", "r")
+        HOST = file.readline().rstrip('\n')
+        PORT = 27017
+        USER = file.readline().rstrip('\n')
+        PASS = file.readline().rstrip('\n')
+        DB = "betsol"
+
+        client = MongoClient(HOST, PORT, username=USER, password=PASS)
+        db = client[DB]
+        print("Success")
+        return db
+    except Exception as e:
+        print("Failed")
+        print(str(e))
+
+
+users = ["Raj Kiran",
+         "Tanupriya Gupta",
+         "Ashwin Bharadhwaj",
+         "Meghashree Maddihally Nagoji",
+         "Krupa Yadiyal",
+         "Rajesh Gangadhara K",
+         "Shyamanth Kumar",
+         "Chaithra R",
+         "Radhika Sadum",
+         "Vishnu Rahul",
+         "Swapna Vasudevamurthy",
+         "Mohan Vishwanath",
+         "Venkateswara Rao",
+         "Shresta Hegde",
+         "Keerthana J",
+         "Vaibhav Deshpande",
+         "Prasannakumar G K",
+         "Gokul Varma",
+         "Firoz S Khan",
+         "SureshKumar Rajendran",
+         "Aman Bhayana",
+         "Sagar S",
+         "Akshay Bokan",
+         "Pavan Kumar A",
+         "Ashish Kumar",
+         "Trupti Pai",
+         "Sushravya M B",
+         "Nishit Shah",
+         "Ashithraj Shetty",
+         "Raghavendra Anegundi",
+         "Sagar Mitkari",
+         "Shankar Tharakeswaran",
+         "Sindhushree V",
+         "Sindhu Srinivasadatta",
+         "Samith Jagannath",
+         "Akshay Aradhya",
+         "Chaitanya Alavandi",
+         "Varsha Sharma",
+         "Sourav Samanta",
+         "Abhijith Arkalgud",
+         "Shrita G",
+         "Karthik Hebbar",
+         "Lavanya N E",
+         "Megha Tyagi",
+         "Neha Aggarwal",
+         "Plabani P Sahoo",
+         "Ujwal P U",
+         "Vikas R",
+         "Biswajit Mohanty",
+         "Nuthan Kumar",
+         "Santhosh S S",
+         "Sreenivas Ramegowda",
+         "Govardhana Shetty",
+         "Prasad Shetty",
+         "Pavan Raj",
+         "Arvind Srinath",
+         "Solomon mark",
+         "Raghavendran Srinivasan",
+         "Prerna Singh",
+         "Allia Khosla",
+         "Shrey Arora",
+         "Prakhar Saxena",
+         "Shrinidhi Kulkarni",
+         "Harshith Maiya",
+         "Suthanth D K"]
+
+db = getMongoDBConnection()
+
+db.secretsanta.drop()
+
+users.sort()
+
+for rowuser in users:
+    santa = {}
+    for coluser in users:
+        if coluser == rowuser:
+            santa[coluser] = False
+        else:
+            santa[coluser] = True
+    db.secretsanta.insert_one({'name': rowuser, 'santa': santa})
