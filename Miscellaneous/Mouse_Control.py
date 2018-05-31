c559dba7b2fff1e77599995991b28a545647d184
DollarAkshay <akshay95aradhya@gmail.com>
2018-05-31 16:26:42 +0530

    Better approach that actually works

--- Miscellaneous/Mouse_Control.py
+++ Miscellaneous/Mouse_Control.py
@@ -1,11 +1,16 @@
 import win32api
 import time
 import math
+import os
+import win32con
 
+aspect_ratio = 16 / 9
 i = 0
 while True:
-    win32api.SetCursorPos((int(math.cos(math.radians(i)) * 100 + 683), int(math.sin(math.radians(i)) * 100 + 381)))
-    time.sleep(0.01)
+    x = int(math.cos(math.radians(i)) * 30000 / aspect_ratio + 65535 / 2)
+    y = int(math.sin(math.radians(i)) * 30000 + 65535 / 2)
+    win32api.mouse_event(win32con.MOUSEEVENTF_ABSOLUTE | win32con.MOUSEEVENTF_MOVE, x, y, 0, 0)
+    win32api.Sleep(10)
     i += 1
     if i % 360 == 0:
-        time.sleep(2)
+        win32api.Sleep(6400)
