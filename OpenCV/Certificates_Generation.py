005393a84544355af334ee651c87f03d5a69977d
DollarAkshay <akshay95aradhya@gmail.com>
2019-08-13 16:37:55 +0530

    Updating Certificate Generator

--- OpenCV/Certificates_Generation.py
+++ OpenCV/Certificates_Generation.py
@@ -3,15 +3,12 @@ import cv2
 import numpy as np
 
 img_width = 1122
-char_width = 22
+char_width = 60
 max_char = 51
 
-image_template = "C:\\Users\\akshay.aradhya\\Pictures\\Graphic Design\\SwiftCode Certificates\\SwiftCode Certificate Template.png"
+image_template = "D:\\Python Programs\\OpenCV\\Images\\Certificate_of_Participation.png"
 
-name_list = ["Abhishek Harsh", "Akanksha Kumari", "Animesh Singh", "Deepak V Kashyap ", "Hardik Asnani",
-             "Kailasa Aravind", "Kashyap Kitchlu", "Macha Pujitha", "Magadal Shriya", "Nagarjun  T N ", "Navneeth Kumar",
-             "Rashmi N", "Sanjna Umesh", "Sankarshan A Joshi", "Sharath S K", "Shubhangi Vasishta", "Suhaib Khan", "Supriya M",
-             "Christeen S", "Raksha A"]
+name_list = ["Sriram"]
 
 date = "06-Oct-2017"
 
@@ -21,17 +18,15 @@ for name in name_list:
     pil_im = Image.fromarray(cv2_im_rgb)
     draw = ImageDraw.Draw(pil_im)
 
-    font_courbd = ImageFont.truetype("courbd.ttf", 36)
-    font_roboto = ImageFont.truetype("Roboto-Light.ttf", 24)
+    font_roboto = ImageFont.truetype("Roboto-Light.ttf", 128)
 
     # Write Name
-    draw.text(((max_char - len(name)) * char_width / 2, 370),
-              name.upper(), font=font_courbd, fill=(0, 0, 0, 255))
+    text_x = 1600 - len(name) * char_width / 2
+    draw.text((text_x, 1600), name.upper(), font=font_roboto, fill=(0, 0, 0, 255))
 
     # Write Date
-    draw.text((810, 575), date, font=font_roboto, fill=(0, 0, 0, 255))
+    # draw.text((810, 575), date, font=font_roboto, fill=(0, 0, 0, 255))
 
     cv2_im_processed = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
 
-    cv2.imwrite('C:\\Users\\akshay.aradhya\\Pictures\\Graphic Design\\SwiftCode Certificates\\BNMIT\\' +
-                name.upper() + ".png", cv2_im_processed)
+    cv2.imwrite('D:\\Python Programs\\OpenCV\\' + name.upper() + ".png", cv2_im_processed)
