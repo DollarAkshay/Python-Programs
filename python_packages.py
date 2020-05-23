67abf99809cf6032ea30d7388aa9a387d3b625ec
DollarAkshay <akshay95aradhya@gmail.com>
2020-05-23 16:21:28 +0530

    Adding the setup script

---
+++ python_packages.py
@@ -0,0 +1,27 @@
+import os
+
+os.system("python -m pip install --upgrade pip")
+
+package_list = [
+    'wheel',
+
+    'beautifulsoup4',
+    'gym',
+    'ipywidgets',
+    'jupyter',
+
+    'matplotlib',
+    'numpy',
+    'opencv-python',
+    'pandas',
+    'pygame',
+
+    'requests',
+    'scikit-learn',
+    'seaborn',
+    'tensorflow-gpu==1.15',
+    'xgboost',
+]
+
+for package in package_list:
+    os.system("python -m pip install -U {}".format(package))
